20748210506a0e9c137ba26ee07eb9d0ac56ef7c
vishalsahilai <vishalsahilofficial@gmail.com>
2026-08-02 13:00:17 -0700

    add code to connect frontend to bankend

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from app.routes import doctor_router, patient_router, staff_router
+from app.database import Base, engine
+from app.models import doctor, patient, staff
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Hospital Management System",
@@ -15,3 +19,12 @@ app.include_router(staff_router)
 @app.get("/")
 def root():
     return {"message": "Hospital Management System API is running"}
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
